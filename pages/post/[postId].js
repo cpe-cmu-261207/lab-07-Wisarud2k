07f1f788c0c6ad2a2eb49cf907f8b25cc980d820
Wisarud2k <59276807+Wisarud2k@users.noreply.github.com>
2021-01-24 23:06:14 +0700

    everthing seem work all that left is apply old portfolio and gpaCal

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -1,16 +1,89 @@
 import {useRouter} from "next/router";
 import {useEffect,useState} from 'react'
 import axios from 'axios'
+import Link from "next/link";
 const baseURL = 'https://dummyapi.io/data/api'
 const Post = () => {
     const router = useRouter()
     const {postId} = router.query
     const [post,setPost] = useState(null)
     const [comment,setComment] = useState([])
+    const fetchPost = async () => {
+        if(postId){
+            const response = await axios.get(`${baseURL}/post/${postId}`,
+            {
+                headers: {
+                    'app-id' : '600d932cbe74672ded2529ad'
+                }
+            })
+            setPost(response.data)
+            console.log(response.data)
+        }
+    }
+    const fetchComment = async () => {
+        if(postId){
+            const response = await axios.get(`${baseURL}/post/${postId}/comment`,
+            {
+                headers: {
+                    'app-id' : '600d932cbe74672ded2529ad'
+                }
+            })
+            setComment(response.data.data)
+            // console.log(response.data.data)
+        }
+    }
 
+    useEffect(fetchPost,[postId])
+    useEffect(fetchComment,[postId])
     return (
         <>
-            <h1>display post data from api here</h1>
+            {/* <h1>display post data from api here</h1> */}
+            <>
+            {post !== null ? (
+                
+                <>
+                    <h1>
+                        post : {post.text}
+                    </h1>
+                    <p>
+                        tags : {post.tags.map(item => (
+                            <>
+                                {item }{' '}
+                            </>
+                        ))} 
+                    </p>
+                    <img width="400px" src= {post.image}></img>
+                    <p>
+                        Like : {post.likes}
+                    </p>
+
+                    
+                </>):null
+                
+            }
+
+            {comment !== null ? (
+                <>
+                    <h5>Comments</h5>
+                    {comment.map(com => (
+                        <>
+                            <p>
+                               {com.owner.firstName} {com.owner.lastName} : {com.message}
+                            </p>
+                        </>
+                    ))
+                    }
+                </>
+            ):null
+            }
+
+            <Link href='/post'>
+                <button>
+                back
+                </button>
+            </Link>
+            
+            </>
         </>
     )
 }
--- pages/post/index.js
+++ pages/post/index.js
@@ -5,10 +5,40 @@ const baseURL = 'https://dummyapi.io/data/api'
 
 const Posts = () => {
     const [posts,setPosts] = useState([])
+    const fetchPosts = async () => {
+        const response = await axios.get(`${baseURL}/post`,
+        {
+            headers: {
+                'app-id': '600d932cbe74672ded2529ad'
+            }
+        })
+        console.log(response.data)
+        setPosts(response.data.data)
+    }
 
+    useEffect(fetchPosts,[])
     return (
         <>
             <h1>All Posts</h1>
+            <>
+                {
+                    posts.map(post => (
+                        <>
+                            <p>
+                                post : {post.text}
+                            </p>
+                            <img width="400px" src= {post.image}></img>
+                            <p>
+                                Like : {post.likes}
+                            </p>
+
+                            <Link key={post.id} href={`/post/${post.id}`}>
+                                <button>Go to this post</button>
+                            </Link>
+                        </>
+                    ))
+                }
+            </>
         </>
     )
 }
